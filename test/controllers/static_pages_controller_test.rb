5c2ac03cb1125b3bbef5e497ea5c6340f5f46316
frank184 <francoisbelanger1993@hotmail.com>
2015-07-23 03:37:57 -0400

    Added Static Pages Views

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home" + append_title
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About" + append_title
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help" + append_title
+  end
+
+  private
+  def append_title
+    return " | Ruby on Rails Sample App"
   end
 
 end
